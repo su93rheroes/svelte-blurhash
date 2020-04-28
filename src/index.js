ce23bb049a3f8192186af2c5b73bd93d3e35baf7
h416 <h416@users.noreply.github.com>
2020-04-29 08:29:26 +0900

    add components

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-export { default as default } from './Component.svelte';
+// export { default as default } from './Component.svelte';
+
+export { Blurhash } from "./Blurhash.svelte";
+export { BlurhashImage } from "./BlurhashImage.svelte";
