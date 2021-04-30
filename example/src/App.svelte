d3739e55558f6f022aca597e29b587ebb8381257
h416 <h416@users.noreply.github.com>
2021-04-30 13:36:10 +0900

    refs #3 picture tag

--- example/src/App.svelte
+++ example/src/App.svelte
@@ -1,19 +1,28 @@
 <script>
-        import BlurhashImage from "svelte-blurhash";
+        import { BlurhashImage, BlurhashPicture } from "svelte-blurhash";
 </script>
 
 <div>
-        
+        <BlurhashPicture
+                src={"https://www.1-firststep.com/samplephp/jpg-webp-avif/image/1000.jpg"}
+                hash={"LEHV6nWB2yk8pyoJadR*.7kCMdnj"}
+                width={320}
+                height={206}
+                fadeDuration={800}
+        >
+                <source
+                        srcset="https://www.1-firststep.com/samplephp/jpg-webp-avif/image/1000.avif"
+                        type="image/avif"
+                />
+        </BlurhashPicture>
 
-        
         <BlurhashImage
                 src={"https://blurha.sh/assets/images/img1.jpg"}
                 hash={"LEHV6nWB2yk8pyoJadR*.7kCMdnj"}
                 width={320}
                 height={206}
-                fadeDuration={200}
+                fadeDuration={800}
         />
-
 
         <BlurhashImage
                 src={"https://blurha.sh/assets/images/img2.jpg"}
@@ -21,8 +30,6 @@
                 width={320}
                 height={206}
         />
-
-
 
         <BlurhashImage
                 src={"https://blurha.sh/assets/images/img3.jpg"}
@@ -38,7 +45,6 @@
                 height={206}
                 fadeDuration={2000}
         />
-
 </div>
 
 <div>
--- test.sh
+++ test.sh
@@ -3,6 +3,7 @@ mkdir test
 cp -r example/* test/
 cp src/*.svelte test/src/ 
 cd test
-sed -i -e "s|svelte-blurhash|./BlurhashImage.svelte|" src/App.svelte
+sed -i -e 's|import { BlurhashImage, BlurhashPicture } from "svelte-blurhash";|import BlurhashImage from "./BlurhashImage.svelte";|' src/App.svelte
+sed -i '2a import BlurhashPicture from "./BlurhashPicture.svelte";'  src/App.svelte
 npm run build
 npm run start
